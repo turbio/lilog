76ec7068e503b5075bf673b31b2ae1ce8aeb1808
turbio <turbio@masonclayton.com>
2016-05-23 13:27:40 -0700

    clf entry parser

--- log_parser/clf.js
+++ log_parser/clf.js
@@ -1,5 +1,20 @@
 module.exports.name = 'clf';
 
 module.exports.parse = function(str){
-	return str + ' parsed';
+	var regex = /([\d\.:a-zA-z]+) (.+?) (.+?) \[(.+?)\] "(\w+) (.+?) (.+?)" (\d{3}) (\d+)/g
+
+	var parts = regex.exec(str);
+	if(parts != null){
+		return {
+			from: parts[1],
+			time: parts[4],
+			path: parts[6],
+			status: parts[8],
+			size: parts[9],
+			verb: parts[5],
+			proto: parts[7],
+		};
+	}else{
+		console.log('error parsing log entry "' + str + '"');
+	}
 };

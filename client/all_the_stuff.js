10bd67d417e05e5c37f28cc906b80da99f25a621
turbio <turbio@masonclayton.com>
2016-05-25 16:38:23 -0700

    no longer using canvas for timeline, css is now more concise

--- client/all_the_stuff.js
+++ client/all_the_stuff.js
@@ -197,56 +197,14 @@ window.onload = function(){
 		var new_client = new client(data.from);
 		var new_request = new request(new_client, new_server);
 		log_entries.push(data);
-		//timeline_update();
 	});
 	socket.on('past', function(data){
 		log_entries = data;
-		//timeline_update();
 	});
 
-	clients_panel = document.getElementById('clients-list');
-	servers_panel = document.getElementById('resources-list');
-
-	timeline = document.getElementById('timeline');
-	timeline.width = timeline.parentElement.offsetWidth;
-	timeline.height = timeline.parentElement.offsetHeight - 30;
-	timeline_ctx = timeline.getContext("2d");
-
-	window.setInterval(timeline_update, timeline_update_interval);
+	clients_panel = document.getElementById('clients');
+	servers_panel = document.getElementById('servers');
 };
-
-function timeline_update(){
-	timeline_ctx.clearRect(0, 0, timeline.width, timeline.height);
-
-	timeline_ctx.beginPath()
-
-	timeline_ctx.strokeStyle = "#fff";
-	timeline_ctx.lineWidth = 1;
-
-	log_entries.forEach(function(request){
-		var seconds_diff = (Date.now() - request.time) / 1000;
-		var lineX = timeline.width - (seconds_diff * 4);
-
-		timeline_ctx.moveTo(lineX, 0);
-		timeline_ctx.lineTo(lineX, timeline.height);
-		timeline_ctx.stroke();
-	});
-
-
-	var x = timeline.width;
-	timeline_ctx.fillStyle = "#fff";
-	while(x >= 0){
-		timeline_ctx.moveTo(x, timeline.height - 10);
-		timeline_ctx.lineTo(x, timeline.height);
-		timeline_ctx.stroke();
-
-		timeline_ctx.fillText(Math.ceil((timeline.width - x) / 2), x + 2, timeline.height - 2);
-
-		x -= 2 * 20;
-	}
-
-	timeline_ctx.closePath()
-}
 
 function elementDim(element){
 	if(element){

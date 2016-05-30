92cba73f095b6d50a0a04077da8955de79ea4c91
turbio <turbio@masonclayton.com>
2016-05-29 21:02:17 -0700

    move material colors over to css

--- public/all_the_stuff.js
+++ public/all_the_stuff.js
@@ -6,25 +6,6 @@ var request_over_wait = 2000;
 var clients_panel = null;
 var servers_panel = null;
 
-var colors = [
-	'#F44336',
-	'#E91E63',
-	'#9C27B0',
-	'#673AB7',
-	'#3F51B5',
-	'#2196F3',
-	'#03A9F4',
-	'#00BCD4',
-	'#009688',
-	'#4CAF50',
-	'#8BC34A',
-	'#CDDC39',
-	'#FFEB3B',
-	'#FFC107',
-	'#FF9800',
-	'#FF5722'
-];
-
 var log_entries = [];
 var requests = [];
 

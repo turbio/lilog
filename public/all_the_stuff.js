6783231c4c87a421e797a5bbf39cba9d3f06c725
turbio <turbio@masonclayton.com>
2016-06-02 10:16:06 -0700

    transition over to using jquery... it's a bit more concise

--- public/all_the_stuff.js
+++ public/all_the_stuff.js
@@ -1,89 +1,102 @@
-var timeline = null;
-var timeline_ctx = null;
-//var timeline_update_interval = 500;
-var timeline_update_interval = 50000;
-var request_over_wait = 2000;
-var clients_panel = null;
-var servers_panel = null;
+//a resource list is a panel containing a list of either servers/clients/etc
+//which can have requests going between them
+//the resource list manages creation and lifetime of all the items within it
+function resource_list(list_element){
+	this.list = [];
+	this.list_element = list_element;
+}
+//the primary external interface to the resource list, all everything should be doing
+//is creating resources. Their deletion is handled by the resource list
+resource_list.prototype.create = function(name){
+	var existing_item = this.find(name);
+	if(existing_item){
+		return existing_item;
+	}
 
-var log_entries = [];
-var requests = [];
+	var available_spaces = this.list_element.find(':empty');
 
-var clients = {
-	find: function(name){
-		return this.arr.find(function(c){
-			return name == c.name;
-		});
-	},
-	remove: function(client){
-		this.arr.splice(this.arr.indexOf(client), 1);
-		while(client.element.firstChild){
-			client.element.removeChild(client.element.firstChild);
-		}
-		while(!clients_panel.lastChild.hasChildNodes()){
-			clients_panel.removeChild(clients_panel.lastChild);
-		}
-	},
-	push: function(client){
-		this.arr.push(client);
-	},
-	get_entry: function(){
-		available = Array.from(clients_panel.children).find(function(l){
-			return !l.hasChildNodes();
-		});
+	var available_space;
+	if(available_spaces.length == 0){
+		available_space = $('<li></li>')
+			.on('animationend', function(event){
+				$(this).removeClass('blink');
+			})
+			.appendTo(this.list_element);
+	}else{
+		available_space = available_spaces.first();
+	}
 
-		if(!available){
-			available = document.createElement('li');
-			available.addEventListener('animationend', function(event){
-				this.classList.remove('blink');
-			});
-			clients_panel.appendChild(available);
-		}
+	var new_item = new resource_item(name, available_space, this);
 
-		return available;
-	},
-	arr: []
+	//this.list_element.height(this.list_element.parent().innerHeight())
+	this.list.push(new_item);
+
+	return new_item;
+}
+//used for resource items upon creation in order to determine if a resource
+//already exists
+resource_list.prototype.find = function(name){
+	return this.list.find(function(r){
+		return name == r.name;
+	});
+}
+//used by resource items when they are being deleted
+//either by themselves or the resource list
+resource_list.prototype.remove = function(resource_item){
+	this.list.splice(this.list.indexOf(resource_item), 1);
+	resource_item.element.empty();
+
+	while(this.list_element.children().last().is(':empty')){
+		this.list_element.children().last().remove();
+	}
+}
+
+//resource items appear visually as items in the resource panel
+//all have a corresponding resource list which managers their creation
+//each resource has a list of corresponding requests
+function resource_item(name, element, parent_list){
+	this.name = name;
+	this.parent_list = parent_list;
+
+	//the number of times this resource has been involved in a request
+	this.count = 1;
+	this.requests = [];
+	this.element = this.createElement(element);
+}
+resource_item.prototype.addRequest = function(request){
+	this.count++;
+	this.requests.push(request);
 };
+resource_item.prototype.removeRequest = function(request){
+	this.requests.splice(this.requests.indexOf(request), 1);
 
-var servers = {
-	find: function(name){
-		return this.arr.find(function(c){
-			return name == c.name;
-		});
-	},
-	remove: function(server){
-		this.arr.splice(this.arr.indexOf(server), 1);
-		while(server.element.firstChild){
-			server.element.removeChild(server.element.firstChild);
-		}
-		while(!servers_panel.lastChild.hasChildNodes()){
-			servers_panel.removeChild(servers_panel.lastChild);
-		}
-	},
-	push: function(server){
-		this.arr.push(server);
-	},
-	get_entry: function(){
-		available = Array.from(servers_panel.children).find(function(l){
-			return !l.hasChildNodes();
-		});
-
-		if(!available){
-			available = document.createElement('li');
-			available.addEventListener('animationend', function(event){
-				this.classList.remove('blink');
-			});
-			servers_panel.appendChild(available);
-		}
-
-		return available;
-	},
-	arr: []
+	if(this.requests.length == 0 ){
+		window.setTimeout(this.remove.bind(this), request_over_wait);
+	}
 };
+resource_item.prototype.createElement = function(element){
+	$('<div>' + this.name + '</div>')
+		.appendTo(element);
+	return element;
 
+};
+resource_item.prototype.remove = function(){
+	//a resource should not be removed while it has active requests
+	if(this.requests.length == 0){
+		this.parent_list.remove(this);
+	}
+}
+resource_item.prototype.blink = function(){
+	this.element.removeClass('blink');
+	this.element.addClass('blink');
+}
+
+//represents a single request between two resources
+//rendered visibly as a moving circle
 function request(from, to, size, verb, status){
 	to.addRequest(this);
 	from.addRequest(this);
+	from.blink();
 
 	this.from = from;
 	this.to = to;
@@ -93,164 +106,100 @@ function request(from, to, size, verb, status){
 
 	this.direction = 'to';
 	this.element = this.createElement();
-
-	requests.push(this);
 }
 request.prototype.createElement = function(){
-	var element = document.createElement('div');
-	element.classList.add('request');
-	element.setAttribute('verb', this.verb);
-	element.setAttribute('size', this.size);
-	element.setAttribute('status', this.status);
+	this.from_position = this.from.element.offset();
+	this.from_position.left += this.from.element.width();
 
-	this.from_position = $(this.from.element).offset();
-	this.from_position.left += $(this.from.element).width();
+	this.to_position = this.to.element.offset();
 
-	this.to_position = $(this.to.element).offset();
-
-	element.style.transform = 'translate('
-		+ this.from_position.left + 'px ,'
-		+ this.from_position.top + 'px)';
+	var element = $('<div></div>')
+		.addClass('request')
+		.attr('verb', this.verb)
+		.attr('size', this.size)
+		.attr('status', this.status)
+		.css('top', this.from_position.top)
+		.css('left', this.from_position.left);
 
 	var self = this;
-	element.addEventListener('DOMNodeInserted', function(){
-		window.setTimeout(function(){
-			element.style.transform = 'translate('
-				+ self.to_position.left + 'px ,'
-				+ self.to_position.top + 'px)';
-		}, 1);
+
+	element.animate({
+		top: this.to_position.top,
+		left: this.to_position.left },
+	1500,
+	function(){
+		self.direction = 'from';
+
+		var size_as_dimensions = (Math.log(self.size + 1) / 2) + 10;
+
+		element.animate({
+			width: size_as_dimensions,
+			height: size_as_dimensions
+		},
+		{
+			duration: 100,
+			queue: false
+		});
+
+		self.to.blink();
 	});
 
-	var self = this;
-	element.addEventListener('transitionend', function(event){
-		if(event.propertyName == 'transform'){
-			self.reachedDestination();
-		}
-	});
+	element.animate({
+		top: this.from_position.top,
+		left: this.from_position.left
+	},
+	request_speed,
+	this.remove.bind(this));
 
-	document.getElementById('lists').appendChild(element);
+	element.appendTo($('#lists'));
+
 	return element;
-};
-request.prototype.direction = 'to';
-request.prototype.reachedDestination = function(){
-	if(this.direction == 'to'){
-		this.direction = 'from';
-
-		this.element.style.transform = 'translate('
-			+ this.from_position.left + 'px ,'
-			+ this.from_position.top + 'px)';
-
-		var size_as_dimensions = (Math.log(this.size + 1) / 2) + 10;
-
-		this.element.style.width = size_as_dimensions + 'px';
-		this.element.style.height = size_as_dimensions + 'px';
-
-		this.to.element.classList.add('blink');
-	}else{
-		this.remove();
-		this.from.element.classList.add('blink');
-	}
 };
 request.prototype.remove = function(){
 	this.to.removeRequest(this);
+	this.from.blink();
 	this.from.removeRequest(this);
-	this.element.parentElement.removeChild(this.element);
-
-	var location = requests.indexOf(this);
-	requests.splice(location, 1);
+	this.element.remove();
 };
 
-function server(name){
-	var existing_server = servers.find(name);
-	if(existing_server){
-		existing_server.requested();
-		return existing_server;
-	}
+var request_speed = 2000;
+var timeline = null;
+var timeline_ctx = null;
+//var timeline_update_interval = 500;
+var timeline_update_interval = 50000;
+var request_over_wait = 2000;
+var servers_panel = null;
 
-	this.name = name;
-	this.count = 1;
-	this.requests = [];
-	this.element = this.createElement();
+var log_entries = [];
 
-	servers.push(this);
+var servers;
+var clients;
+
+var timeline = {
+	left: 10 * 1000,
+	right: 0,
+	max: 0,
+	new: function(){
+
+	},
+	entries: []
+};
+
+function timeline_entry(data){
+	var entry = $('<div></div>')
+		.addClass('timeline-point')
+		.attr('verb', data.verb)
+		.css('right', ((Date.now() - data.time) / 250) + 'px')
+		.appendTo(timeline);
 }
-server.prototype.addRequest = function(request){
-	this.requests.push(request);
-};
-server.prototype.removeRequest = function(request){
-	this.requests.splice(this.requests.indexOf(request), 1);
-	if(this.requests.length == 0 ){
-		window.setTimeout(this.remove.bind(this), request_over_wait);
-	}
-};
-server.prototype.createElement = function(){
-	var text = document.createElement('div');
-	text.innerHTML = this.name;
-
-	var entry = servers.get_entry();
-	entry.appendChild(text);
-
-	return entry;
-};
-server.prototype.remove = function(){
-	if(this.requests.length == 0){
-		servers.remove(this);
-	}
-}
-server.prototype.requested = function(){
-	this.count++;
-};
-
-function client(name){
-	var existing_client = clients.find(name);
-	if(existing_client){
-		existing_client.requested();
-		return existing_client;
-	}
-
-	this.name = name;
-	this.count = 1;
-	this.requests = [];
-	this.element = this.createElement();
-
-	clients.push(this);
-}
-client.prototype.addRequest = function(request){
-	this.requests.push(request);
-};
-client.prototype.removeRequest = function(request){
-	this.requests.splice(this.requests.indexOf(request), 1);
-
-	if(this.requests.length == 0 ){
-		window.setTimeout(this.remove.bind(this), request_over_wait);
-	}
-};
-client.prototype.createElement = function(){
-	var text = document.createElement('div');
-	text.innerHTML = this.name;
-
-	var entry = clients.get_entry();
-	entry.appendChild(text);
-
-	return entry;
-};
-client.prototype.remove = function(){
-	if(this.requests.length == 0){
-		//while the removal animation is playing, it should already be considered
-		//to not exist
-		clients.remove(this);
-	}
-}
-client.prototype.requested = function(){
-	this.count++;
-};
 
 $(function(){
 	var socket = io.connect('http://' + window.location.host);
 	socket.on('new', function(data){
 		if(data){
-			var new_server = new server(data.path);
-			var new_client = new client(data.from);
+			var new_server = servers.create(data.path);
+			var new_client = clients.create(data.from);
+
 			var new_request = new request(
 				new_client,
 				new_server,
@@ -258,12 +207,15 @@ $(function(){
 				data.verb,
 				data.status);
 			log_entries.push(data);
+			timeline_entry(data);
 		}
 	});
 	socket.on('past', function(data){
 		log_entries = data;
 	});
 
-	clients_panel = document.getElementById('clients');
-	servers_panel = document.getElementById('servers');
+	servers = new resource_list($('#servers'));
+	clients = new resource_list($('#clients'));
+
+	timeline = document.getElementById('timeline');
 });

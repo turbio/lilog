b1341ee709d7f88f2ed1b7375e6b4f6f9b21cef9
turbio <masongclayton@gmail.com>
2016-10-11 14:14:13 -0700

    fix some style errors

--- log_parser/clf.js
+++ log_parser/clf.js
@@ -1,23 +1,24 @@
 module.exports.name = 'clf';
 
-module.exports.parse = function(str){
-	var parse_line = /([\d\.:a-zA-z]+) ([^ ]+) ([^ ]+?) \[([^\]]+)\] "([^ ]+) ([^ ]+) ([^ ]+)" (\d{3}) (\d+)/g
+module.exports.parse = (logLine) => {
+	const parseLineRegex = /([\d\.:a-zA-z]+) ([^ ]+) ([^ ]+?) \[([^\]]+)\] "([^ ]+) ([^ ]+) ([^ ]+)" (\d{3}) (\d+)/g;
 
-	var parse_date = /^(\d+)\/(\w+)\/(\d+):(\d{2}:\d{2}:\d{2}) (.+)$/g;
-	var format_date = '$1 $2 $3 $4 GMT$5';
+	const parseDateRegex = /^(\d+)\/(\w+)\/(\d+):(\d{2}:\d{2}:\d{2}) (.+)$/g;
+	const dateFormat = '$1 $2 $3 $4 GMT$5';
 
-	var parts = parse_line.exec(str);
-	if(parts != null){
-		return {
-			from: parts[1],
-			time: Date.parse(parts[4].replace(parse_date, format_date)) / 1000,
-			path: parts[6],
-			status: parts[8],
-			size: parts[9],
-			verb: parts[5],
-			proto: parts[7],
-		};
-	}else{
-		console.log("error parsing log entry '" + str + "'");
+	const parts = parseLineRegex.exec(logLine);
+
+	if (!parts) {
+		return null;
 	}
+
+	return {
+		from: parts[1],
+		time: Date.parse(parts[4].replace(parseDateRegex, dateFormat)) / 1000,
+		path: parts[6],
+		status: parts[8],
+		size: parts[9],
+		verb: parts[5],
+		proto: parts[7],
+	};
 };

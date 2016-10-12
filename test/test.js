54814e65f809a476ef92b37f2e336952142cdd19
turbio <masongclayton@gmail.com>
2016-10-12 14:56:38 -0700

    some basic tests

--- test/test.js
+++ test/test.js
@@ -2,21 +2,62 @@ const loggen = require('./loggen');
 const fs = require('fs');
 const io = require('socket.io-client');
 const chai = require('chai');
+const request = require('request');
+
+const Server = require('../server');
+const Log = require('../log');
 
 chai.should();
 
 const port = 3000;
-const logFilePath = './testlogfile';
 
-const serverUrl = `http://127.0.0.1:${port}`;
+//const logFilePath = './testlogfile';
 
-const options ={
-	transports: ['websocket'],
-	'force new connection': true
-};
+//const serverUrl = `http://127.0.0.1:${port}`;
 
-describe('lilog server', () => { });
+//const options ={
+	//transports: ['websocket'],
+	//'force new connection': true
+//};
 
-describe('lilog logger', () => { });
+describe('lilog server', () => {
+	let server = null;
+
+	before(() => {
+		server = new Server(port);
+		server.start();
+	});
+
+	it('should serve index page', (done) => {
+		request(`http://localhost:${port}`, (err, res) => {
+			res.body.should.match(/<html/);
+			res.body.should.match(/<head/);
+			res.body.should.match(/<body/);
+			done(err);
+		});
+	});
+
+	it('should serve static css', (done) => {
+		request(`http://localhost:${port}/style.css`, (err, res) => {
+			res.body.should.match(/\w.+ {.+}/);
+			done(err);
+		});
+	});
+});
+
+describe('lilog logger', (done) => {
+	let log = null;
+
+	before(() => {
+		log = new Log();
+		log.loadParsers().then(done);
+	});
+
+	it('should list all parsers', () => {
+		log.listParsers()
+			.map((parser) => parser.name)
+			.should.contain('clf');
+	});
+});
 
 describe('lilog', () => { });
